95dbd585df657c9f6e85a2a74998caf4b931be44
limebeck <mail@limebeck.dev>
2024-07-15 21:57:00 +0300

    Update versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,17 @@
 rootProject.name = "microservice-utils"
 
 
-pluginManagement {
-    plugins {
-        val kotlinVersion: String by settings
-        kotlin("multiplatform") version kotlinVersion
-    }
-}
+//pluginManagement {
+//    plugins {
+//        val kotlinVersion: String by settings
+//        kotlin("multiplatform") version kotlinVersion
+//    }
+//}
 
 rootProject.projectDir.resolve("libs")
     .listFiles { it, _ -> it.isDirectory }
     ?.forEach {
-        if(it.resolve("build.gradle.kts").exists()) {
+        if (it.resolve("build.gradle.kts").exists()) {
             include("libs:${it.name}")
         }
     }
